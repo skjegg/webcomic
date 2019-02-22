d995b04db9572ff61906de50373935e7671a79a0
Joakim Monstad <jmonstad@cimpress.com>
2019-02-22 14:43:11 -0500

    looping and cleanup

--- crispifyS3TOFolder.js
+++ crispifyS3TOFolder.js
@@ -5,17 +5,14 @@ var suffix = ".jpg";
 // Get token from https://developer.cimpress.io/apis/graphics/imagemind?version=v2&environment=Prod%20V2#/Crispify/post_v2_imagemind_crispify
 var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik1qbENNemxCTnpneE1ETkJSVFpHTURFd09ETkRSalJGTlRSR04wTXpPRUpETnpORlFrUTROUSJ9.eyJodHRwczovL2NsYWltcy5jaW1wcmVzcy5pby93YXMiOlsiYWRmc3xqbW9uc3RhZEBjaW1wcmVzcy5jb20iXSwiaHR0cHM6Ly9jbGFpbXMuY2ltcHJlc3MuaW8vY2ltcHJlc3NfaW50ZXJuYWwiOnRydWUsImh0dHBzOi8vY2xhaW1zLmNpbXByZXNzLmlvL3RlbmFudHMiOlsiY2ltcHJlc3MiXSwiaHR0cHM6Ly9jbGFpbXMuY2ltcHJlc3MuaW8vZW1haWwiOiJqbW9uc3RhZEBjaW1wcmVzcy5jb20iLCJodHRwczovL2NsYWltcy5jaW1wcmVzcy5pby9jYW5vbmljYWxfaWQiOiJqbW9uc3RhZEBjaW1wcmVzcy5jb20iLCJpc3MiOiJodHRwczovL2NpbXByZXNzLmF1dGgwLmNvbS8iLCJzdWIiOiJ3YWFkfGlUMVc5Q1AybndwUWY4YkhWY1Y4WUlrWjFMYnp5dTVLc05tcjdPSUZLdkkiLCJhdWQiOlsiaHR0cHM6Ly9hcGkuY2ltcHJlc3MuaW8vIiwiaHR0cHM6Ly9jaW1wcmVzcy5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNTUwODQ2OTc1LCJleHAiOjE1NTA4NjEzNzUsImF6cCI6IkcxN0hkTmQwMWdBUGZpU1Y1dXBiV2RpRFVuQVU4aXM5Iiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCJ9.IBLXW0pBMpsKUVfj_5eSzO4-QZzYNSEYjXBqG45PyAUo3Kr4XEf-tIgXdkGIFrBHYcP5sTuts6KkRLUzUpYhcQorUtww9ndPbm90nvM_77sx9x26AgQ-agjzM58FhuVisIhskzwMXuzqOEki3wKQ6VL7Ee-i6YWBu26sGeDBfVz-_VALu81CsGTygp61wYBgZNCwOv1dHQYpTTthsgRUOcKg8LrmEHKxQcpzWDbP62K_-Tox9CToKOy7898BPq3wjZorpCiTCxTroc1KOM2msJ_3w88oTsdaCFg8SXBX6d7JUB4GV3NRiRYSQiCzj7vlk8UrKjV9Hygz3PG27GPF3Q";
 var crispifyEndPoint = "https://imagemind.ipa.cimpress.io/v2/imagemind/crispify"
-
 var crispedDestination = "crispyimages/";
 
-var http = require('http');
+
 var fs = require('fs');
 var request = require('request')
 
 
-var crisped = {};
-
-var crispit = function (sourceurl) {
+var crispit = function (sourceurl, current) {
     request.post(crispifyEndPoint, 
     {
        json: { 
@@ -34,7 +31,7 @@ var crispit = function (sourceurl) {
         console.log(body)
         debugger;
         console.log(body.output.url);
-        download(body.output.url, crispedDestination+'test'+suffix);
+        download(body.output.url, crispedDestination+current+suffix);
     }).auth('','',true, token);
 
 }
@@ -42,27 +39,12 @@ var crispit = function (sourceurl) {
 var download = function(url, dest){
     request.get(url).pipe(fs.createWriteStream(dest));
 }
-/*
-var download = function (url, dest, cb) {
-    var file = fs.createWriteStream(dest);
-    var request = http.get(url, function (response) {
-        response.pipe(file);
-        file.on('finish', function () {
-            file.close(cb);
-        });
-    });
-};*/
 
-crispit(sourceBaseUrl +current +suffix);
-
-/*
 var more = true;
 while (more) {
-    download(baseUrl + current + suffix, destination + current + suffix);
-
+    crispit(sourceBaseUrl +current +suffix, current);
     current++;
     if (current > stop) {
         more = false;
     }
 }
-*/
